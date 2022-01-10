d4bdd5966bc62b14e731793930aeaa919a43249d
Sohailshah20 <contact.sohailali@gmail.com>
2022-01-10 21:03:13 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/jetpackcomponents/ExpandableCard.kt
@@ -0,0 +1,112 @@
+package com.example.jetpackcomponents
+
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerBasedShape
+import androidx.compose.material.*
+import androidx.compose.material.SnackbarDefaults.backgroundColor
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import com.example.jetpackcomponents.ui.theme.Shapes
+
+
+@ExperimentalMaterialApi
+@Composable
+fun ExpandableCard(
+    title: String,
+    titleFontSize: TextUnit = MaterialTheme.typography.h6.fontSize,
+    titleFontWeight: FontWeight = FontWeight.Bold,
+    description: String,
+    descriptionFontSize: TextUnit = MaterialTheme.typography.subtitle1.fontSize,
+    descriptionFontWeight: FontWeight = FontWeight.Normal,
+    descriptionMaxLines: Int = 4,
+    shape: CornerBasedShape = Shapes.medium,
+    padding: Dp = 12.dp,
+    cardColor: Color = Color.White
+
+){
+     var expandedState by remember { mutableStateOf(false) }
+     val rotateState by animateFloatAsState(
+         targetValue = if (expandedState) 180f else 0f)
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .animateContentSize(
+                animationSpec = tween(
+                    durationMillis = 300,
+                    easing = LinearOutSlowInEasing
+                )
+
+            ),
+        shape = shape,
+        backgroundColor = cardColor,
+        onClick = { expandedState = !expandedState }
+        )
+    {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(padding)
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    modifier = Modifier
+                        .weight(6f),
+                    text = title,
+                fontSize = titleFontSize,
+                fontWeight = titleFontWeight,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis) //Replaces text with ... if exceeds the limit
+
+                IconButton(
+                    modifier = Modifier
+                        .alpha(ContentAlpha.medium)
+                        .weight(1f)
+                        .rotate(rotateState),
+                    onClick = {
+                        expandedState = !expandedState
+                    }
+                ) {
+                    Icon(imageVector = Icons.Default.ArrowDropDown,
+                        contentDescription = null )
+                }
+            }
+            if(expandedState){
+                Text(text = description,
+                fontSize = descriptionFontSize,
+                    fontWeight = descriptionFontWeight,
+                maxLines = descriptionMaxLines,
+                overflow = TextOverflow.Ellipsis)
+            }
+             
+        }
+    }
+}
+
+@ExperimentalMaterialApi
+@Preview
+@Composable
+fun ExpandedCardPreview(){
+    ExpandableCard(title = "My Title", description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
+}
